067134f5a11bde9654954e09bb1374336635aa70
aberML <lvchuan@hotmail.com>
2019-10-10 14:31:35 +0100

    change github link to raw file

--- startup.sh
+++ startup.sh
@@ -7,5 +7,5 @@ yes | pip3 install --user virtualenv
 ~/Library/Python/3.7/bin/virtualenv env
 source env/bin/activate
 pip install jupyter pycryptodomex
-curl -OL 'https://github.com/aberML/CSM3120/blob/master/Crypto.ipynb'
+curl -OL 'https://raw.githubusercontent.com/aberML/CSM3120/master/Crypto.ipynb'
 jupyter notebook Crypto.ipynb
